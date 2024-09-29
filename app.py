c26da1068092d57ec33e36ddd8a9bf8529ffea32
saidakbardev005 <saidakbarxoja14@gmail.com>
2024-09-29 10:51:53 +0500

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import io
 #temp=pathlib.PosixPath
 #pathlib.PosixPath=pathlib.WindowsPath
 plt=platform.system()
-if plt =="Linux":
+if plt =="Linux" or plt == "Darwin":
   pathlib.WindowsPath=pathlib.PosixPath
 # title
 st.title("Transportni klassifikatsiya qiluvchi model")
